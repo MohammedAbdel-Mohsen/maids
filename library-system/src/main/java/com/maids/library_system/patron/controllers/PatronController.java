0b5de603b01631ff4063daafc1a4db843929b6d6
mohamed.elnawam <mohamed.elnawam@systemsltd.com>
2024-08-16 22:16:39 +0300

    Committing Enhancements - Documenting the rest APIs

--- library-system/src/main/java/com/maids/library_system/patron/controllers/PatronController.java
+++ library-system/src/main/java/com/maids/library_system/patron/controllers/PatronController.java
@@ -2,19 +2,16 @@ package com.maids.library_system.patron.controllers;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.maids.library_system.patron.models.request.PatronReqModel;
 import com.maids.library_system.patron.models.response.PatronResModel;
@@ -23,40 +20,67 @@ import com.maids.library_system.patron.services.PatronService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/patrons")
+@RequestMapping("/patron")
 @Validated
 @AllArgsConstructor
+@Tag(name = "Patron", description = "The Patron API")
 public class PatronController {
 
-	PatronService patronService;
+    PatronService patronService;
 
-	
-	@PostMapping
-	public ResponseEntity<Long> createPatron(
-		    @RequestBody @Valid PatronReqModel patronReqModel) {
-		return new ResponseEntity<>(patronService.createPatron(patronReqModel), HttpStatus.OK);
-	}
-	
-	
-	@PutMapping(path ="/{id}")
-	public ResponseEntity<Long> updatePatron(@PathVariable("id") long patronId,
-		     @RequestBody @Valid PatronReqModel patronReqModel) {
-		return new ResponseEntity<>(patronService.updatePatron(patronId,patronReqModel), HttpStatus.OK);
-	}
-	
-	@GetMapping(path ="/{id}")
-	public ResponseEntity<PatronResModel> getPatronById(@PathVariable("id") long patronId) {
-		return new ResponseEntity<>(patronService.getPatronById(patronId), HttpStatus.OK);
-	}
-	
-	@GetMapping
-	public ResponseEntity<List<PatronResModel>> getAllPatrons() {
-		return new ResponseEntity<>(patronService.getAllPatrons(), HttpStatus.OK);
-	}
-	
-	@DeleteMapping(path="/{id}")
-	public ResponseEntity<Void> deletePatronById(@PathVariable("id") long patronId){
-		patronService.deletePatronById(patronId);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+    @PostMapping
+    @Operation(summary = "Create a new patron")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patron successfully created"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Server error"),
+    })
+    public ResponseEntity<Long> createPatron(@RequestBody @Valid PatronReqModel patronReqModel) {
+        return new ResponseEntity<>(patronService.createPatron(patronReqModel), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    @Operation(summary = "Update an existing patron")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patron successfully updated"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Patron not found"),
+            @ApiResponse(responseCode = "500", description = "Server error"),
+    })
+    public ResponseEntity<Long> updatePatron(@PathVariable("id") long patronId, @RequestBody @Valid PatronReqModel patronReqModel) {
+        return new ResponseEntity<>(patronService.updatePatron(patronId, patronReqModel), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    @Operation(summary = "Get a patron by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved patron"),
+            @ApiResponse(responseCode = "404", description = "Patron not found"),
+            @ApiResponse(responseCode = "500", description = "Server error"),
+    })
+    public ResponseEntity<PatronResModel> getPatronById(@PathVariable("id") long patronId) {
+        return new ResponseEntity<>(patronService.getPatronById(patronId), HttpStatus.OK);
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all patrons")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all patrons"),
+            @ApiResponse(responseCode = "500", description = "Server error"),
+    })
+    public ResponseEntity<List<PatronResModel>> getAllPatrons() {
+        return new ResponseEntity<>(patronService.getAllPatrons(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Delete a patron by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted patron"),
+            @ApiResponse(responseCode = "404", description = "Patron not found"),
+            @ApiResponse(responseCode = "500", description = "Server error"),
+    })
+    public ResponseEntity<Void> deletePatronById(@PathVariable("id") long patronId) {
+        patronService.deletePatronById(patronId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
